92a13c548f3702b51b176d34c261257eb5c57609
Lizzie Siegle <esiegle@brynmawr.edu>
2024-06-20 14:38:48 -0700

    first commit hi craig

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,49 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from  "hono"
+type Env = {
+	AI : Ai
+}
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Env}>();
+
+function createGist(title, code, fileName) {
+
+}
+
+app.post('/bait', async (c) => {
+	//const payload = await c.req.json();
+	const answer = await c.env.AI.run(
+		'@hf/nousresearch/hermes-2-pro-mistral-7b',
+		{
+			messages: [{role: "system", content: "You are a software engineer social media influencer known for writing bad code in order to get more comments and increase engagement"}, { role: "user", content: "Create ternary example in JavaScript" }],
+			tools: [
+			  {
+				name: "createGist",
+				description: "Create a GitHub Gist in a specific programming language",
+				parameters: {
+				  type: "object",
+				  properties: {
+					title: {
+					  type: "string",
+					  description: "The title of the Gist",
+					},
+					code: {
+					  type: "string",
+					  description: "The code for the Gist",
+					},
+					fileName: {
+						type: "string",
+						description: "The file name of the Gist"
+					}
+				  },
+				  required: ["title", "code", "fileName"],
+				},
+			  },
+			],
+		});
+	  console.log(`answer ${answer}`);
+	  return c.json(answer);
+
+})  
+
+
+export default app;
